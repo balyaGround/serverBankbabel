ae5b873928e30b9fb1f4b633b60632eecf410084
gilang.sukma <gilang.sukma@privyid.tech>
2022-02-22 22:13:14 +0700

    change: move parameter to admin folder

--- routes/admin/parameter.js
+++ routes/admin/parameter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const route = express.Router();
 
-const { getParameter, createParameter } = require("../controller/parameter");
+const { getParameter, createParameter } = require("../../controller/parameter");
 
 route.get("/", getParameter);
 route.post("/", createParameter);
