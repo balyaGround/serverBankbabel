85c37b6415c66cfaddc679861aeb2dd9f94dcd74
gilang.sukma <gilang.sukma@privyid.tech>
2022-02-22 22:11:53 +0700

    add: user models

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      requires: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      set: setPassword,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      required: true,
+      default: "user",
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+    toObject: { getters: true },
+    toJSON: { getters: true },
+  }
+);
+
+function setPassword(password) {
+  return bcrypt.hashSync(password, 10);
+}
+
+userSchema.plugin(mongooseDelete, { overrideMethods: "all" });
+module.exports = mongoose.model("user", userSchema);
